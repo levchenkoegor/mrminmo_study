bf93b22d2d722e953484685c05156992898654dc
elevchenko <levchenkoegors@gmail.com>
2025-03-13 00:03:29 +0000

    bbregister script

---
+++ activemotion_study/10_run_bbregister.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Setup freesurfer for bbregister
+export FREESURFER_HOME=/tools/freesurfer
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
+echo $(freesurfer --version)
+
+export SUBJECTS_DIR=/data/elevchenko/MinMo_movements/activemotion_study/derivatives/freesurfer
+export data_folder=/data/elevchenko/MinMo_movements/activemotion_study/derivatives
+
+# Extract subject IDs dynamically, removing 'sub-' prefix and appending '_nii'
+subjects=$(ls $data_folder | grep -oP '^sub-\K24\d{4}[A-Z]{2}')
+
+for subj in "sub-241031DC"; do #$subjects; do
+  for cond in "MinMo" "NoMinMo"; do
+
+    subj_preproc_outputs=${data_folder}/${subj}_nii/${subj}_nii_task-mvts_cond-${cond}/${subj}_nii_task-mvts_cond-${cond}.results
+
+    3dcalc -a ${subj_preproc_outputs}/vr_base+orig. -expr 'a' -prefix ${subj_preproc_outputs}/vr_base.nii.gz
+
+    bbregister --s $subj \
+               --mov ${subj_preproc_outputs}/vr_base.nii.gz \
+               --reg ${subj_preproc_outputs}/${subj}_bbreg.dat \
+               --T2
+ done
+done
